dfe7581f9cab9e6c422415610db61fe551f9ae2e
Nichita Gutu <gutu.nichita08@gmail.com>
2025-04-29 09:10:07 +0200

    ajout des fichiers a jour

--- game/Morpion/ClientMorpion.java
+++ game/Morpion/ClientMorpion.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.net.*;
 
 public class ClientMorpion {
-    private static final int PORT = 5000;
+    private static final int PORT = 8080;
 
     public static void main(String[] args) {
         final String SERVER_ADDRESS = args[0];
